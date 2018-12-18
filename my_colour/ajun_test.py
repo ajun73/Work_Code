b68d41f48210561790dc4104abaa7aa34fe19540
jun <ajun73@gmail.com>
2018-12-19 01:14:56 +0900

    DIR 변경 COLOUR_LASTEST -> COLOUR_RECENT

---
+++ my_colour/ajun_test.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import colour
+
+import colour.plotting
+#from colour.plotting import (colour_plotting_defaults,single_illuminant_relative_spd_plot,)
+
+#from colour.utilities import message_box
+
+#message_box('Plot test')
+
+colour.plotting.colour_plotting_defaults()
+
+colour.plotting.single_illuminant_relative_spd_plot('F1')
+
